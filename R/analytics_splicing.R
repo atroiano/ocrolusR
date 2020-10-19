c849085d47d9a5a71d52bdce2b7fae01270b2359
Andy Troiano <atroiano@PGADATAANAL01.can.com>
2020-10-19 15:28:07 -0400

    * created new function to get negative dates * fixed function for get_min_max_by_month to check if a column exists before trying to parse it

--- R/analytics_splicing.R
+++ R/analytics_splicing.R
@@ -49,11 +49,13 @@ get_min_max_by_month = function(df,col_name){
     df_unnested
   })
   month_data = month_data %>% reduce(bind_rows)
-
+  month_data_cols = colnames(month_data)
   if(nrow(month_data)>0){
-    month_data  = month_data %>% mutate(
-      txn_date = as.Date(as.POSIXct(txn_date, format = '%m/%d/%Y'))
-    )
+    if('txn_date' %in% month_data_cols ){
+      month_data  = month_data %>% mutate(
+        txn_date = as.Date(as.POSIXct(txn_date, format = '%m/%d/%Y'))
+      )
+    }
   }
   month_data
 }
@@ -79,4 +81,17 @@ unnest_analytics_df= function(df,col)
 {
   df %>% unnest(cols={{col}}) %>% select_if(~!is.list(.x))
 
+}
+#' Get Negative Value Dates
+#' @param df df with the columns to grab
+#' @param col the column to extract
+#' @return df with the selected columns
+#' @export
+get_negative_balance <- function(df,col_name){
+  month_cols = df %>% select(contains(col_name)) %>% colnames
+  df_data = df
+  month_data = map(month_cols, function(x,df=df_data){
+    df_unnested = df %>% select(x) %>% pull(x) %>% compact
+  })
+  month_data = month_data %>% reduce(c) %>% enframe %>% unnest(value) %>% select(negative_dates = value)
 }
